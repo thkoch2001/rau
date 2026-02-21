518312fbf1a3720b047a4a22aa87cbaf4035e8da
tazjin <mail@tazj.in>
2026-02-21 19:31:47 +0300

    lisp: ensure that each frame has a matchable title

--- lisp/reka.el
+++ lisp/reka.el
@@ -1,4 +1,5 @@
 (require 'libreka)
+(require 'cl-lib)
 
 (defgroup reka nil
   "Reka - Emacs swimming in the river"
@@ -8,8 +9,16 @@
 (defcustom reka-handle nil
   "Opaque handle for interacting with the WM")
 
+(defun reka--ensure-frame-names ()
+  "Ensure each frame has a unique title that reka can match to its xwindow."
+  (cl-loop for frame being the frames
+           do (unless (string-prefix-p "reka-frame-"
+                                       (frame-parameter frame 'name))
+                (set-frame-parameter frame 'name (make-temp-name "reka-frame-")))))
+
 (defun reka-enable ()
   (message "Launching native module ...")
+  (reka--ensure-frame-names)
   (setq reka-handle (reka-start-wm)))
 
 (defun reka-handle-sigusr1 ()
@@ -19,8 +28,5 @@
     (reka-send-command reka-handle "whatever")))
 
 (define-key special-event-map [sigusr1] #'reka-handle-sigusr1)
-
-(defun reka-test ()
-  (reka-send-command reka-handle "test"))
 
 (provide 'reka)
