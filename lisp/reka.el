978d499dd4ac926d7a293d54b3aa2e6e0b40df79
Vincent Ambo <mail@tazj.in>
2026-03-16 23:10:37 +0300

    refactor(reka): more convenient intercept prefix pushing
    Splitting out this function makes it possible to bind new things in a
    slightly easier way.
    
    Change-Id: I463e308ddc2e48dce58aab7e5d6dd8a870ffcba9
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/14093
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- lisp/reka.el
+++ lisp/reka.el
@@ -197,10 +197,13 @@ The Rust side resolves the keysyms using xkbcommon."
          (key (if (characterp basic) basic (symbol-name basic))))
     (list event key (apply #'logior mods))))
 
+(defun reka-push-intercept-prefix (prefix &optional command)
+  (let ((data (reka--key-to-xkb prefix)))
+    (reka-register-xkb-prefix reka-handle (car data) (cadr data) (caddr data) command)))
+
 (defun reka-push-intercept-prefixes ()
   (dolist (prefix reka-intercept-prefixes)
-    (let ((data (reka--key-to-xkb prefix)))
-      (reka-register-xkb-prefix reka-handle (car data) (cadr data) (caddr data) nil))))
+    (reka-push-intercept-prefix prefix)))
 
 (defun reka--suppress-focus-event (_orig-fn _event)
   "No-op for suppressing certain focus events in advice."
